120dbc623e570cde7c4ae8bae376bf2c65de5bc6
archwine <7epimenides@gmail.com>
2025-01-27 19:46:11 +0800

    add cuda device prop directory

---
+++ cudaDeviceProp/cudaDeviceProp.cu
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+
+int main() {
+    int deviceCount;
+    cudaGetDeviceCount(&deviceCount);
+
+    if (deviceCount == 0) {
+        std::cout << "No CUDA devices found." << std::endl;
+        return 1;
+    }
+
+    for (int i = 0; i < deviceCount; i++) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, i);
+
+        std::cout << "Device " << i << ": " << deviceProp.name << std::endl;
+
+        std::cout << "  Total Global Memory: " << deviceProp.totalGlobalMem << " bytes" << std::endl;
+        std::cout << "  Max Threads Per Block: " << deviceProp.maxThreadsPerBlock << std::endl;
+        std::cout << "  Max Threads Per Multiprocessor: " << deviceProp.maxThreadsPerMultiProcessor << std::endl;
+        std::cout << "  Register Per Block: " << deviceProp.regsPerBlock << std::endl;
+        std::cout << "  Shared Memory per Block: " << deviceProp.sharedMemPerBlock << " bytes" << std::endl;
+        std::cout << "  Warp Size: " << deviceProp.warpSize << std::endl;
+        std::cout << "  Register Per Multiprocessor: " << deviceProp.regsPerMultiprocessor << std::endl;
+        std::cout << "  Shared Memory per Block Optin: " << deviceProp.sharedMemPerBlockOptin << " bytes" << std::endl;
+        std::cout << "  Shared Memory per Multiprocessor: " << deviceProp.sharedMemPerMultiprocessor << " bytes" << std::endl;
+    }
+}
